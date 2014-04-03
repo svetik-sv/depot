d18e8d20b8a36b6797e33135a24d14fb4492a6ab
Sveta <skutina-sv@yandex.ru>
2014-04-03 21:02:47 +0800

    message

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'fae1f148854e8f891e67ee5968af3019aa22e416cb91ddf339639dff1c7c234c5f0ab1a70b1e6069cebca83ceaad0eddcbe2bb2f8b53f590d90d593134a1815d'
---
+++ test/models/product_test.rb
@@ -0,0 +1,62 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  test "свойства товара не должны оставаться пустыми" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "цена товара должна быть положительной" do
+    product = Product.new(title: "my book title",
+                          description: "yyy",
+                          image_url: "qwe.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "должна быть больше или равна 0,01",
+    product.errors[:price].join('; ')
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "должна быть больше или равна 0,01",
+    product.errors[:price].join('; ')
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "my book title",
+                description: "qqq",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "url изображения" do
+    ok=%w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+           http://a.b.c/x/y/z/fred.gif}
+    bad=%w{ fred.doc fred.gif/more fred.gif.more}
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} не должно быть неприемлемым"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} не должно быть приемлемым"
+    end
+  end
+
+  test "если у товара нет уникального названия то он недопустим" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "www",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert !product.save
+    assert_equal "уже было использовано", product.errors[:title].join('; ')
+  end
+
+end
