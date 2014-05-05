59261bb8ba491427eb711495d3f2ee4112da4f2c
Sveta <skutina-sv@yandex.ru>
2014-05-05 19:32:39 +0800

    глава 11 упражнения

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Depot::Application.routes.draw do
-  resources :line_items
 
   resources :carts
 
   get "store/index"
   resources :products
+
+  resources :line_items do
+    #member do
+    #  put 'decrement'
+    #end
+    put 'decrement', on: :member
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
