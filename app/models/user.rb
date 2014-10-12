a36e4747f4b0f9049afabad5fb228c9960cb84a8
gafar <r125644@gmail.com>
2014-10-12 13:02:59 +0800

    Minor improvement

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
   private
   def ensure_an_admin_remains
     if User.count.zero?
-      raise "Последний пользоваьель не может быть удален"
+      raise "Последний пользователь не может быть удален"
     end
   end
 end
